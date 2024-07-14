b3a826305f0288164e1a82d4e5f73cc424c96d9f
ACEXRA <81796112+ACEXRA@users.noreply.github.com>
2024-07-14 17:55:22 +0530

    Update firebase.js

--- src/components/config/firebase.js
+++ src/components/config/firebase.js
@@ -7,15 +7,7 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyATDEIsPI6C7BzAFkRBvRaiJgrgra1julE",
-  authDomain: "react-1f64c.firebaseapp.com",
-  databaseURL: "https://react-1f64c-default-rtdb.firebaseio.com",
-  projectId: "react-1f64c",
-  storageBucket: "react-1f64c.appspot.com",
-  messagingSenderId: "627332414417",
-  appId: "1:627332414417:web:8f6ba41fccc26a4b833ef0",
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
